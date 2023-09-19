99aba5778b564c3fceebf6dab2bfdeb64ed6982d
Sano <ordysan@gmail.com>
2023-09-19 02:35:00 +0200

    create the project, main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { NgIf } from '@angular/common';
+import { Time } from './model/time';
+
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, HttpClientModule,MatCardModule, MatButtonModule, NgIf],
 })
-export class AppComponent {
-  title = 'eos-pace';
+export class AppComponent implements OnInit {
+  public title:string = 'EoS Calculator';
+  public showResults:boolean = false;
+  public pst_time:string;
+  public resets_left:number = 0;
+  public days_left:number = 0;
+  public current_ex:number;
+  public current_score:number;
+  public exercises_left:number;
+  public pace:number[] = [0,0,0,0];
+  private eos_score:number = 0;
+  private current_hour:number;
+  private bonus:number = 0;
+  private res:Time;
+
+  timeUrl:string = 'https://worldtimeapi.org/api/timezone/America/Los_Angeles';
+
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(): void {
+    this.http.get<Time>(this.timeUrl, {observe: 'body', responseType: 'json'})
+      .subscribe((data) => {
+        this.res = data;
+        this.pst_time = new Date(this.res.datetime).toLocaleTimeString('en', { hour12: false, timeZone: 'US/Pacific' });
+      });
+  }
+
+  calculateDaysLeft() {
+    this.days_left = this.res.week_number%2 ? 14-parseInt(this.res.day_of_week, 10) : 7-parseInt(this.res.day_of_week, 10);
+  }
+
+  calculateResetsLeft() {
+    this.current_hour = parseInt(new Date(this.res.datetime).toLocaleTimeString('en', { hour: '2-digit', hour12: false, timeZone: 'US/Pacific' }), 10);
+    if (this.current_hour >= 18)
+      this.resets_left = 0
+    else if (this.current_hour >= 12)
+      this.resets_left = 1
+    else
+      this.resets_left = 2
+    this.resets_left += this.days_left*3;
+  }
+  
+  calculateExercisesLeft() {
+    this.exercises_left = this.resets_left*5 + +this.current_ex;
+  }
+
+  calculatePace() {
+    this.calculateDaysLeft();
+    this.calculateResetsLeft();
+    this.calculateExercisesLeft();
+    this.eos_score = +this.current_score + +(10*this.exercises_left);
+    this.bonus = this.current_ex > 0 ? 1 : 0;
+    this.pace[0] = this.eos_score - this.resets_left*1 - this.bonus;
+    this.pace[1] = this.eos_score;
+    this.pace[2] = this.eos_score + this.resets_left*1 + this.bonus;
+    this.pace[3] = this.eos_score + this.resets_left*2 + this.bonus*2;
+    this.showResults = true;
+  }
+
+  isValid() {
+    if (this.current_ex !== undefined && this.current_score !== undefined){
+      return !isNaN(this.current_ex) && !isNaN(this.current_score) ? true : false;
+    } else
+      return false;
+  }
 }
